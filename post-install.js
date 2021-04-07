0c76b9cda3d59b3dbd95e7d199c76702f6513cb4
Claudemiro Junior <claudemiro@gruporecursos.intranet>
2021-04-07 14:32:35 -0300

    testando ENV

---
+++ post-install.js
@@ -0,0 +1,25 @@
+const { exec } = require('child_process');
+
+let command;
+
+if (process.env.ENV === 'prod') {
+    command = exec('ng build --aot --configuration=prod');
+} else if (process.env.ENV === 'dev') {
+    command = exec('ng build --aot --configuration=dev');
+}
+
+if (command != undefined) {
+    command.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    command.stderr.on('data', (data) => {
+        console.error(data);
+    });
+
+    command.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+} else {
+    console.error('process.env.ENV: ' + process.env.ENV);
+}
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_TICKET: 'https://sca-dev-api.herokuapp.com',
+};
